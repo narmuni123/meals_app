84f6430daa7041836c82fdc3230182dfe0f5875b
Munikiran Narpareddi <narmuni123@gmail.com>
2022-01-17 22:47:48 +0530

    created a grid view

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
+import 'package:meal_app/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 /2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Munikiran Meal"),
+        ),
+        body: GridView(
+          children: DUMMY_CATEGORIES.map((catData) => CategoryItem(
+            title: catData.title,
+            color: catData.color,
+          )).toList(),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 /2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+        ),
       ),
     );
   }
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
-  const CategoryItem({Key? key}) : super(key: key);
+  final String title;
+  final Color color;
+
+  CategoryItem({this.title = "", this.color = Colors.blue});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        color: color,
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
   }
 }
